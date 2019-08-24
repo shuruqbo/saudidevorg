6fd2b7e1282aeefe76d3b74bace25a8799f9c3e8
shuruqbo <52832443+shuruqbo@users.noreply.github.com>
2019-08-25 00:49:22 +0300

    day 3

---
+++ day3.py
@@ -0,0 +1,10 @@
+x,y,z = "red", "yellow" , "blue"
+print(x)
+print(y)
+print(z)
+
+one = uno = 1
+print(one)
+
+SDO = "Saudi developers organization"
+print ("Im enjoying " + SDO + " 100 day of coding") 
