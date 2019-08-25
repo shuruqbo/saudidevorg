49237bf8ec2626b1cd99b665b9d74ea039daf38b
shuruqbo <52832443+shuruqbo@users.noreply.github.com>
2019-08-25 12:22:59 +0300

    day5

---
+++ day5.py
@@ -0,0 +1,7 @@
+# a code to calculate the number of the variables 
+
+x , y , z = "apple" , "orange" , "limon"
+print("the number of letters in the following words are")
+print(" apple, orange and limon ")
+print("equals : " ) 
+print(len(x) + len(y) + len(z))
