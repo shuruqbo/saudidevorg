76527bf6d24baaff06f13dce846e6c56b18789d4
shuruqbo <52832443+shuruqbo@users.noreply.github.com>
2019-08-29 17:13:56 +0300

    lesson 9 and day 10

---
+++ day10.py
@@ -0,0 +1,29 @@
+# arithmetic operators  
+x = 5
+y = 7
+
+print( x + y )
+print( x - y )
+print( x * y / 2 )
+print( x ** y )
+print( x // y )
+print( x % y )
+
+# assignement operators 
+t = 120
+t /= 40
+m = 4
+m += 8
+
+print(t)
+print(m)
+
+# comparsion operators
+
+a = 5
+b = 1
+c = 1.0
+d = 3
+print( a != d )
+print( b == c)
+print(c > d)
---
+++ lesson9.py
@@ -0,0 +1,17 @@
+a = " how old are you? {}"
+b = 25
+print(a.format(b))
+
+c = "I bought {} apple and {} oranges"
+app = 2
+ora = 3
+print(c.format(app, ora))
+
+numbers = "the first 4 numbers are {0} , {1} , {2}, {3} "
+
+d = 1
+e = 2
+f = 3
+g = 4
+
+print(numbers.format(d, e, f, g))
