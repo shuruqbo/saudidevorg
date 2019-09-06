2a57e301b6f9c9224a21afabb8dd2534cb7a2385
shuruqbo <52832443+shuruqbo@users.noreply.github.com>
2019-09-06 03:06:34 +0300

    Create lesson17.py

---
+++ lesson17.py
@@ -0,0 +1,17 @@
+tuple2 =( "cookies" , "waffles" , "pancake")
+print(tuple2)
+
+if "cookies" in tuple2:
+	print( "yes" )
+
+tuple3 = ("fruits", "vegitables")
+
+tuple1 = (tuple2 + tuple3)
+print(tuple1)
+print(len(tuple1))
+
+# example 
+x = (3,4,5,6)
+x = x + (1,2,3)
+print(x)
+
