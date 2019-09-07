5ee19aecdc9c3f3030e83a37d2ffa2608a0bf8f2
shuruqbo <52832443+shuruqbo@users.noreply.github.com>
2019-09-07 23:30:28 +0300

    day18&19

---
+++ day18.py
@@ -0,0 +1,28 @@
+# weekly challenge 
+#use four methods on a list:
+list1 = ["flashlight", 'torch', "lamp", "lantern"]
+print("Original list: ")
+print(list1)
+
+# 1.adding an item
+list1.append("sunlight")
+print("Adding an item")
+print(list1)
+print("--")
+
+# 2.sorting items in the list
+list1.sort()
+print("after sorting the items out")
+print(list1)
+print("--")
+# 3. using pop() to remove the last item
+list1.pop()
+print("using pop() with no index will delete the last item")
+print(list1)
+print("--")
+# 4. using the reverse() method 
+print(list1)
+print("after the reverse")
+list1.reverse()
+print(list1)
+
---
+++ day19.py
@@ -0,0 +1,12 @@
+# weekly challenge part2
+
+tuple1 = tuple(("Java","Python","Swift"))
+print(tuple1)
+print("----")
+
+print("is 'Python' an item in the tuple? ")
+
+if "Python" in tuple1:
+	print(" Yes, it is")
+if "Python"	not in tuple1:
+	print("No, it is not")
