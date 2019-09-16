32a76eff4c2fab99ce10e3815365f79c332eab96
shuruqbo <52832443+shuruqbo@users.noreply.github.com>
2019-09-16 23:47:38 +0300

    day 27

---
+++ day27.py
@@ -0,0 +1,7 @@
+# a code to dtremine the type pf the number
+x = float(input(" enter a number:" ))
+
+if x >= 0:
+    print(" the number is postive") 
+else: 
+    print(" the number is negative")    
